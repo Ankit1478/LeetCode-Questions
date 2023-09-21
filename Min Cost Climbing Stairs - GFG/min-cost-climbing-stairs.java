bf6a4e7aa9e620689262067c66bfad720defc45c
Ankit Raj <119005879+Ankit1478@users.noreply.github.com>
2023-09-21 20:45:09 +0530

    Added solution - LeetHub

--- Min Cost Climbing Stairs - GFG/min-cost-climbing-stairs.java
+++ Min Cost Climbing Stairs - GFG/min-cost-climbing-stairs.java
@@ -32,18 +32,19 @@ class GFG {
 class Solution {
     static int minCostClimbingStairs(int[] cost , int N) {
         //Write your code here
-        int dp[]= new int[N+1];
-        Arrays.fill(dp,-1);
-        return  Math.min(f(0, cost, N, dp), f(1, cost, N, dp));
-    }
-    static int f(int idx,int arr[],int n,int dp[]){
-        if(idx>=n){
-            return 0;
+         int n = cost.length;
+        int[] dp = new int[n + 1];
+        
+        // Base cases: cost to reach step 0 and step 1 is 0.
+        dp[0] = 0;
+        dp[1] = 0;
+        
+        // Calculate the minimum cost for each step starting from step 2.
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
         }
-        if(dp[idx]!=-1)return dp[idx];
-        int jump1= f(idx+1,arr,n,dp)+Math.abs(arr[idx]);
-        int jump2=f(idx+2,arr,n,dp)+Math.abs(arr[idx]);
-            
-        return dp[idx]=Math.min(jump1,jump2);
+        
+        // The minimum cost to reach the top will be in dp[n].
+        return dp[n];
     }
 };
