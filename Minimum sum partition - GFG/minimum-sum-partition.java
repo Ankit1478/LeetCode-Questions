4d1c31d19dcb94443e85d7b90098e9caf3200fb8
Ankit Raj <119005879+Ankit1478@users.noreply.github.com>
2023-10-03 01:17:55 +0530

    Added solution - LeetHub

---
+++ Minimum sum partition - GFG/minimum-sum-partition.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n = sc.nextInt();
+                    int A[] = new int[n];
+                    for(int i = 0;i<n;i++)
+                        A[i] = sc.nextInt();
+                    Solution ob = new Solution();
+                    System.out.println(ob.minDifference(A,n));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+
+	public int minDifference(int arr[], int n) 
+	{ 
+	 
+        int totSum = 0;
+
+        // Calculate the total sum of the array elements
+        for (int i = 0; i < n; i++) {
+            totSum += arr[i];
+        }
+
+        // Create a DP table to store subset sum information
+        boolean[][] dp = new boolean[n][totSum + 1];
+
+        // Initialize the DP table for the first row
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+
+        // Initialize the DP table for the first column
+        if (arr[0] <= totSum) {
+            dp[0][totSum] = true;
+        }
+
+        // Fill in the DP table using bottom-up dynamic programming
+        for (int ind = 1; ind < n; ind++) {
+            for (int target = 1; target <= totSum; target++) {
+                // Calculate if the current element is not taken
+                boolean notTaken = dp[ind - 1][target];
+
+                // Calculate if the current element is taken
+                boolean taken = false;
+                if (arr[ind] <= target) {
+                    taken = dp[ind - 1][target - arr[ind]];
+                }
+
+                // Update the DP table for the current element and target sum
+                dp[ind][target] = notTaken || taken;
+            }
+        }
+
+        int mini = Integer.MAX_VALUE;
+
+        // Find the minimum absolute difference between two subsets
+        for (int i = 0; i <= totSum; i++) {
+            if (dp[n - 1][i]) {
+                int diff = Math.abs(i - (totSum - i));
+                mini = Math.min(mini, diff);
+            }
+        }
+        return mini;
+
+	} 
+}
