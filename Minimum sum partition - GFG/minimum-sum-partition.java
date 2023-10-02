1d537ac3068abafb9f1139749520e67c514f80ae
Ankit Raj <119005879+Ankit1478@users.noreply.github.com>
2023-10-03 01:23:29 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.java
+++ Minimum sum partition - GFG/minimum-sum-partition.java
@@ -32,52 +32,41 @@ class Solution
 	{ 
 	 
         int totSum = 0;
-
-        // Calculate the total sum of the array elements
+      
         for (int i = 0; i < n; i++) {
             totSum += arr[i];
         }
 
-        // Create a DP table to store subset sum information
         boolean[][] dp = new boolean[n][totSum + 1];
 
-        // Initialize the DP table for the first row
         for (int i = 0; i < n; i++) {
             dp[i][0] = true;
         }
 
-        // Initialize the DP table for the first column
         if (arr[0] <= totSum) {
-            dp[0][totSum] = true;
+            dp[0][arr[0]] = true;
         }
 
-        // Fill in the DP table using bottom-up dynamic programming
         for (int ind = 1; ind < n; ind++) {
             for (int target = 1; target <= totSum; target++) {
-                // Calculate if the current element is not taken
                 boolean notTaken = dp[ind - 1][target];
-
-                // Calculate if the current element is taken
                 boolean taken = false;
                 if (arr[ind] <= target) {
                     taken = dp[ind - 1][target - arr[ind]];
                 }
-
-                // Update the DP table for the current element and target sum
                 dp[ind][target] = notTaken || taken;
             }
         }
 
-        int mini = Integer.MAX_VALUE;
+        int min = Integer.MAX_VALUE;
 
-        // Find the minimum absolute difference between two subsets
-        for (int i = 0; i <= totSum; i++) {
-            if (dp[n - 1][i]) {
+        for (int i = 0; i <= totSum / 2; i++) {
+            if (dp[n - 1][i] == true) {
                 int diff = Math.abs(i - (totSum - i));
-                mini = Math.min(mini, diff);
+                min = Math.min(min, diff);
             }
         }
-        return mini;
 
+        return min;
 	} 
 }
