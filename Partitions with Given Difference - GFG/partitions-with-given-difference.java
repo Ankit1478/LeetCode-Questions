9c90320e470b1de0d5ce179fe8b608f3351385a6
Ankit Raj <119005879+Ankit1478@users.noreply.github.com>
2023-10-03 23:51:51 +0530

    Added solution - LeetHub

--- Partitions with Given Difference - GFG/partitions-with-given-difference.java
+++ Partitions with Given Difference - GFG/partitions-with-given-difference.java
@@ -52,35 +52,30 @@ class Solution{
     for(int row[]: dp)
     Arrays.fill(row,-1);
         
-     return sum(n-1,s2,arr,dp);
+     return sum(arr,s2,n);
 }
-    public static int sum(int idx,int target,int[] arr,int[][]dp){
+    public static int sum(int arr[],int d,int n){
         
-        // tabulation base case 
-        // int dp[][] = new int[n][d+1];
-        // if(nums[0]==0) dp[0][0] = 2;
-        // dp[0][0] =1;
+       
+        int dp[][] = new int[n][d+1];
+        if(arr[0]==0) dp[0][0] = 2;
+        else dp[0][0] =1;
         
-       // if(nums[0]!=0 && nums[0]<= target) dp[0][nums[0]] =1;
+        if(arr[0]!=0 && arr[0]<= d) dp[0][arr[0]] =1;
         
-		if(idx==0){
-			if(arr[0]==0 && target==0){
-				return 2;
-			}
-			else if(target==arr[0] || target==0){
-				return 1;
-			}else{
-				 return 0;
-			}
-		}
-		 if(dp[idx][target]!=-1)return dp[idx][target];
-		 
-		int nottaken=sum(idx-1, target, arr, dp);
-		int taken=0;
-		if(arr[idx]<=target){
-			taken+=sum(idx-1, target-arr[idx], arr, dp);
-		}
-		return dp[idx][target]=(nottaken+taken)%mod;
+	
+		 for(int i=1;i<n;i++){
+		     for(int j=0;j<=d;j++){
+		         
+		         int notake = dp[i-1][j];
+		         int take = 0;
+		         if(arr[i]<= j){
+		             take = dp[i-1][j-arr[i]];
+		         }
+		         dp[i][j] = (take+notake)%mod;
+		     }
+		 }
+		 return dp[n-1][d];
 	}
 }
 
