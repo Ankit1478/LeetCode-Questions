34941a61987eb65820e2f268c7e4dfa9cfa120e9
Ankit Raj <119005879+Ankit1478@users.noreply.github.com>
2023-10-13 13:32:47 +0530

    Added solution - LeetHub

--- Form a palindrome - GFG/form-a-palindrome.java
+++ Form a palindrome - GFG/form-a-palindrome.java
@@ -34,24 +34,26 @@ class Solution{
         
     int n=str1.length();
     int m=str2.length();
-    int dp[][]=new int[n+1][m+1];
+    int prev[]=new int[m+1];
     
     for(int i=0;i<=n;i++){
-        dp[i][0] = 0;
+        prev[0] = 0;
     }
     for(int i=0;i<=m;i++){
-        dp[0][i] = 0;
+        prev[i] = 0;
     }
     
     for(int ind1=1;ind1<=n;ind1++){
+        int curr[]=new int[m+1];
         for(int ind2=1;ind2<=m;ind2++){
             if(str1.charAt(ind1-1)==str2.charAt(ind2-1))
-                dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+                curr[ind2] = 1 + prev[ind2-1];
             else
-                dp[ind1][ind2] = 0 + Math.max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+                curr[ind2] = 0 + Math.max(prev[ind2],curr[ind2-1]);
         }
+        prev = curr;
     }
-    return dp[n][m];
+    return prev[m];
     }
     public int longestPalinSubseq(String S)
     {
