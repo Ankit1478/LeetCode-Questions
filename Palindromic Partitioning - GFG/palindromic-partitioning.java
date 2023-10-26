61d40041b9f1c53cc78992976b3072b11557f8b6
Ankit Raj <119005879+Ankit1478@users.noreply.github.com>
2023-10-27 02:02:11 +0530

    Added solution - LeetHub

---
+++ Palindromic Partitioning - GFG/palindromic-partitioning.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String str = in.readLine();
+            Solution ob = new Solution();
+            System.out.println(ob.palindromicPartition(str));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int palindromicPartition(String str)
+    {
+       int n = str.length();
+        int[] dp = new int[n + 1];
+        dp[n] = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            int minCost = Integer.MAX_VALUE;
+            // String[i...j]
+            for (int j = i; j < n; j++) {
+                if (isPalindrome(i, j, str)) {
+                    int cost = 1 + dp[j + 1];
+                    minCost = Math.min(minCost, cost);
+                }
+            }
+            dp[i] = minCost;
+        }
+        return dp[0] - 1;
+    }
+    static int f(int i,int n,String s,int dp[]){
+        if(i==n)return 0;
+        int min =Integer.MAX_VALUE;
+        if(dp[i]!=-1)return dp[i];
+        for(int j=i;j<n;j++){
+            if(isPalindrome(i,j,s)){
+                int cost = 1+ f(j+1,n,s,dp);
+                min = Math.min(min,cost);
+            }
+        }
+        return dp[i]= min;
+    }
+     static boolean isPalindrome(int i, int j, String s) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
