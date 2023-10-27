e486a83e74f54015c87ad6d61bf40a910478d2a7
Ankit Raj <119005879+Ankit1478@users.noreply.github.com>
2023-10-28 03:02:27 +0530

    Added solution - LeetHub

--- String Subsequence - GFG/string-subsequence.java
+++ String Subsequence - GFG/string-subsequence.java
@@ -26,25 +26,45 @@ class Solution {
         // code here 
          int n = s1.length();
         int m = s2.length();
-        return f(n , m , s1, s2);
+       
+        int dp[][] = new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                int nottake =dp[i-1][j];
+               int take = 0;
+
+               if (s1.charAt(i-1) == s2.charAt(j-1)) {
+                    take = dp[i-1][j-1];
+                      }
+
+               dp[i][j] = take + nottake;
+              }
+        }
+        return dp[n][m];
     }
 
-    static int f(int i, int j, String s1, String s2,int dp[][]) {
-        if (j == 0) {
-            return 1; // If s2 is empty, there's one valid subsequence (empty subsequence).
-        }
+    // static int f(int i, int j, String s1, String s2,int dp[][]) {
+    //     if (j == 0) {
+    //         return 1; // If s2 is empty, there's one valid subsequence (empty subsequence).
+    //     }
 
-        if (i == 0) {
-            return 0; // If s1 is empty and s2 is not, there are no valid subsequences.
-        }
+    //     if (i == 0) {
+    //         return 0; // If s1 is empty and s2 is not, there are no valid subsequences.
+    //     }
+        
+    //     if(dp[i][j]!=-1)return dp[i][j];
 
-        int nottake = f(i - 1, j, s1, s2);
-        int take = 0;
+    //     int nottake = f(i - 1, j, s1, s2);
+    //     int take = 0;
 
-        if (s1.charAt(i-1) == s2.charAt(j-1)) {
-            take = f(i - 1, j - 1, s1, s2);
-        }
+    //     if (s1.charAt(i-1) == s2.charAt(j-1)) {
+    //         take = f(i - 1, j - 1, s1, s2);
+    //     }
 
-        return take + nottake;
-    }
+    //     return take + nottake;
+    // }
 }
