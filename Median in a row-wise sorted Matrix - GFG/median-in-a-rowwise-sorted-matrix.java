48580424e24de501977a7b9e47be33c394e4dea5
Ankit Raj <119005879+Ankit1478@users.noreply.github.com>
2023-11-02 16:52:38 +0530

    Added solution - LeetHub

---
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String S[] = read.readLine().split(" ");
+            int R = Integer.parseInt(S[0]);
+            int C = Integer.parseInt(S[1]);
+            int matrix[][] = new int[R][C];
+            int c = 0;
+            for(int i = 0; i < R; i++){
+                String line[]=read.readLine().trim().split(" ");
+                for(int j = 0; j < C; j++){
+                    matrix[i][j] = Integer.parseInt(line[j]);
+                }
+            }
+            Solution ob = new Solution();
+            int ans = ob.median(matrix, R, C);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public static int countSmallerThanMid(int[] A, int mid, int n) {
+    int l = 0, h = n - 1;
+    while (l <= h) {
+      int md = (l + h) >> 1;
+      if (A[md] <= mid) {
+        l = md + 1;
+      } else {
+        h = md - 1;
+      }
+    }
+    return l;
+  }
+    int median(int A[][], int row, int col) {
+    int low = 1;
+    int high = 1000000000;
+    int n = row;
+    int m = col;
+    while (low <= high) {
+      int mid = (low + high) >> 1;
+      int cnt = 0;
+      for (int i = 0; i < n; i++) {
+        cnt += countSmallerThanMid(A[i], mid, col);
+      }
+      if (cnt <= (n * m) / 2)
+        low = mid + 1;
+      else
+        high = mid - 1;
+    }
+    return low;
+}
+}
