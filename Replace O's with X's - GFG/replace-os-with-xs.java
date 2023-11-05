5fd115a996361d3785d685ece5511fa82f60bf8b
Ankit Raj <119005879+Ankit1478@users.noreply.github.com>
2023-11-05 21:41:27 +0530

    Added solution - LeetHub

--- Replace O's with X's - GFG/replace-os-with-xs.java
+++ Replace O's with X's - GFG/replace-os-with-xs.java
@@ -40,72 +40,53 @@ class GFG{
 //User function Template for Java
 
 class Solution{
-    static class Pair {
-        int row;
-        int col;
-
-        Pair(int row, int col) {
-            this.row = row;
-            this.col = col;
+    static void dfs(int row,int col,int n, int m,boolean vis[][] ,char ch[][],int delrow[],int delcol[]){
+        vis[row][col]=true;
+        
+        for(int i=0;i<4;i++){
+            int nrow= row+ delrow[i];
+            int ncol = col+ delcol[i];
+            
+            if(nrow>=0 && ncol>=0 && ncol<m && nrow<n && vis[nrow][ncol]==false  && ch[nrow][ncol]=='O'){
+                dfs(nrow,ncol,n,m,vis,ch,delrow,delcol);
+            }
         }
     }
-
-    static char[][] fill(int n, int m, char[][] mat) {
-        int[] dx = {-1, 0, 0, 1};
-        int[] dy = {0, -1, 1, 0};
-        Queue<Pair> q = new LinkedList<>();
-        int[][] vis = new int[n][m];
-        char[][] ans = new char[n][m];
-        
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(vis[i], -1);
-            Arrays.fill(ans[i], 'X');
-        }
-        
-        for (int i = 0; i < m; i++) {
-            if (mat[0][i] == 'O') {
-                q.add(new Pair(0, i));
-            }
-            if (mat[n - 1][i] == 'O') {
-                q.add(new Pair(n - 1, i));
-            }
-        }
-        
-        for (int i = 1; i < n - 1; i++) {
-            if (mat[i][0] == 'O') {
-                q.add(new Pair(i, 0));
-            }
-            if (mat[i][m - 1] == 'O') {
-                q.add(new Pair(i, m - 1));
-            }
-        }
-        
-        while (!q.isEmpty()) {
-            Pair xx = q.poll();
-            int x = xx.row, y = xx.col;
-            ans[x][y] = 'O';
-            vis[x][y] = 1;
-            
-            for (int i = 0; i < 4; i++) {
-                int newx = x + dx[i];
-                int newy = y + dy[i];
-                
-                if (newx >= 0 && newy >= 0 && newx < n && newy < m && vis[newx][newy] == -1 && mat[newx][newy] == 'O') {
-                    q.add(new Pair(newx, newy));
+    static char[][] fill(int n, int m, char ch[][])
+    {
+        // code here
+       boolean vis[][]= new boolean [n][m];
+       int delrow[] ={0,0,-1,1};
+       int delcol[]={1,-1,0,0};
+       
+       for(int i=0;i<m;i++){
+           if(ch[0][i]=='O' && !vis[0][i]){
+               dfs(0,i,n,m,vis,ch,delrow,delcol);
+           }
+           
+           if(ch[n-1][i]=='O' && !vis[n-1][i] ){
+               dfs(n-1,i,n,m,vis,ch,delrow,delcol);
+           }
+       }
+       
+       
+       for(int i=0;i<n;i++){
+           if(ch[i][0]=='O' && !vis[i][0] ){
+               dfs(i,0,n,m,vis,ch,delrow,delcol);
+           }
+           
+           if(ch[i][m-1]=='O' && !vis[i][m-1] ){
+               dfs(i,m-1,n,m,vis,ch,delrow,delcol);
+           }
+       }
+       
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j] && ch[i][j]=='O'){
+                    ch[i][j]='X';
                 }
             }
         }
-        
-        return ans;
+        return ch;
     }
 }
-
-
-
-
-
-
-
-
-
-
