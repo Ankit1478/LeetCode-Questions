b1f25e719cd8f462ae032756a09bf7ef38e42fbe
Ankit Raj <119005879+Ankit1478@users.noreply.github.com>
2023-11-06 11:22:40 +0530

    Added solution - LeetHub

--- Number Of Enclaves - GFG/number-of-enclaves.java
+++ Number Of Enclaves - GFG/number-of-enclaves.java
@@ -34,64 +34,92 @@ class GFG {
 // } Driver Code Ends
 
 
-
-
 // User function Template for Java
 
 class Solution {
-
-      // Your code hereclass Solution {
-    static class pair{
-        int first;
-        int second;
-        pair(int first,int second){
-            this.first=first;
-            this.second=second;
+    static class Pair{
+        int row;
+        int col;
+        Pair(int row,int col){
+            this.row=row;
+            this.col=col;
         }
     }
 
     int numberOfEnclaves(int[][] grid) {
-        int n =grid.length;
-        int m =grid[0].length;
-        Queue<pair>q = new LinkedList<>();
-        int vis[][]= new int[n][m];
-        // for first row,col and last row,col
+        int n = grid.length;
+        int m = grid[0].length;
+        boolean vis[][] = new boolean[n][m];
+        
+        Queue<Pair>q =new LinkedList<>();
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(i==0 ||j==0||i==n-1 ||j==m-1){
-                    if(grid[i][j]==1){
-                        q.add(new pair(i,j));
-                        vis[i][j]=1;
+                if(i==0 || j==0 || i==n-1 || j==m-1){
+                    if(!vis[i][j] && grid[i][j]==1){
+                        q.add(new Pair(i,j));
+                        vis[i][j] = true;
                     }
                 }
             }
         }
-        int delrow[]={-1,0,1,0};
-        int delcol[]={0,-1,0,1};
         while(!q.isEmpty()){
-            int row=q.peek().first;
-            int col=q.peek().second;
-            q.remove();
-
-            // tarnverse all four direction
+            int row= q.peek().row;
+            int col = q.peek().col;
+            
+            q.poll();
+            
+            int dr[] = {0,0,-1,1};
+            int dc[] ={-1,1,0,0};
+            
             for(int i=0;i<4;i++){
-                int nrow=row+delrow[i];
-                int ncol =col+delcol[i];
-
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                    q.add(new pair(nrow,ncol));
-                    vis[nrow][ncol]=1;
+                int nrow = row+dr[i];
+                int ncol = col+dc[i];
+                
+                if(nrow>=0 && ncol>=0 && ncol<m && nrow<n && !vis[nrow][ncol] && grid[nrow][ncol]==1){
+                    q.add(new Pair(nrow,ncol));
+                    vis[nrow][ncol] =true;
                 }
             }
         }
-        int count=0;
+        
+        int count =0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1 && vis[i][j]==0){
+                if(!vis[i][j] && grid[i][j] ==1){
+                    vis[i][j]=true;
                     count++;
                 }
             }
         }
         return count;
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
