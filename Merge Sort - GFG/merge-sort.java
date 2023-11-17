57dbbb4fb419e7a6317611aa9070985946a9314c
Ankit Raj <119005879+Ankit1478@users.noreply.github.com>
2023-11-17 16:59:51 +0530

    Added solution - LeetHub

---
+++ Merge Sort - GFG/merge-sort.java
@@ -0,0 +1,116 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Merge_Sort
+{
+    //method to print the elements of the array
+	static void printArray(int arr[])
+    {
+        StringBuffer sb=new StringBuffer("");
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            sb.append(arr[i]+" ");
+        System.out.println(sb.toString());
+    }
+
+    
+
+	public static void main(String args[])
+	{
+	    //taking input using Scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		//taking testcases
+		int T = sc.nextInt();
+		while(T>0)
+		{
+		    //taking elements count
+			int n = sc.nextInt();
+			
+			//creating an object of class Merge_Sort
+			Merge_Sort ms = new Merge_Sort();
+			
+			//creating an array of size n
+			int arr[] = new int[n];
+			
+			//adding elements to the array
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+
+            
+			Solution g = new Solution();
+			
+			//calling the method mergeSort
+			g.mergeSort(arr,0,arr.length-1);
+
+            //calling the method printArray
+			ms.printArray(arr);
+		T--;
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+
+
+class Solution
+{
+    void merge(int arr[], int si, int mid, int ei)
+    {
+        int temp[]= new int[ei-si+1];
+        int i=si;
+        int j=mid+1;
+        int k=0;
+        while(i<=mid && j<=ei){
+            if(arr[i]<arr[j]){
+                temp[k++]=arr[i++];
+            }
+            else{
+                temp[k++]=arr[j++];
+            }
+        }
+        while(i<=mid){
+             temp[k++]=arr[i++];
+        }
+        while(j<=ei){
+             temp[k++]=arr[j++];
+        }
+        for(k=0,i=si;k<temp.length;i++,k++){
+            arr[i]=temp[k];
+        }
+    }
+    void mergeSort(int arr[], int si, int ei)
+    {
+         // BASE CASE
+        if (si >= ei) {
+            return;
+        }
+        int mid=si+(ei-si)/2;
+        mergeSort(arr,si,mid);
+        mergeSort(arr,mid+1,ei);
+        merge(arr,si,mid,ei);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
